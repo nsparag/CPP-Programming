f3b02660b7ceb3921f4b24b4225cc0aad1e2d477
Parag Narkhede <ns.parag@gmail.com>
2023-07-11 10:41:40 +0530

    Create helloWorld.cpp

---
+++ helloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// use names for objects and variables from the standard library
+using namespace std;
+
+// program entry point
+int main()
+{
+        cout << "Hello world" << endl;	// display "Hello world"
+}
