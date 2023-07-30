6fa5989a4123293650c80deae1b6fceeebb3a0ce
Parag Narkhede <ns.parag@gmail.com>
2023-07-30 16:14:50 +0530

    Create readWord.cpp

---
+++ string/readWord.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+    char strVariable[100];
+
+    //read and display a word
+    cout << "Enter a string (word): ";
+    cin >> strVariable;
+    cout << "You entered: " << strVariable << endl;    
+
+
+    return 0;
+}
