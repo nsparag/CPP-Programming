582b609aae7f9430bd46f4577541cab1a05e2147
Parag Narkhede <ns.parag@gmail.com>
2024-07-04 15:59:06 +0530

    Create constantVariable.cpp

---
+++ 01_Basics/constantVariable.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+       // Declaring constant Vaiable
+       const float PI = 3.14;  // PI will always be 3.14
+        // PI = 5;           This line will generate error as PI variable is declared as constant and hence it is read only
+       cout << PI << endl;
+
+}
