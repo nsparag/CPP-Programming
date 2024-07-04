b00c9d8b85f54d568f4b57406d82ecad63132b47
Parag Narkhede <ns.parag@gmail.com>
2024-07-04 15:58:08 +0530

    Create variableAndDataTypes.cpp

---
+++ 01_Basics/variableAndDataTypes.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        //Declaring and initializing variable of different data types
+        int myIntVar = 45;              // Integer (whole number without decimals)
+        float myFloatVar = 23.4;        // Floating point number (with decimals)
+        double myDoubleVar = 19.98;     // Floating point number (with decimals)
+        char myCharVar = '#';           // Character
+        string myStringVar = "Hello";   // String (text)
+        bool myBoolVar = true;          // Boolean (true or false)
+
+        // printing the values of variables
+        cout << "The value of myIntVar is " << myIntVar <<endl;
+        cout << "The value of myFloatVar is " << myFloatVar <<endl;
+        cout << "The value of myDoubleVar is " << myDoubleVar <<endl;
+        cout << "The value of myCharVar is " << myCharVar <<endl;
+        cout << "The value of myStringVar is " << myStringVar <<endl;
+        cout << "The value of myBoolVar is " << myBoolVar <<endl;
+}
+
+/* ********************************* Sample Outut ******************************** 
+The value of myIntVar is 45
+The value of myFloatVar is 23.4
+The value of myDoubleVar is 19.98
+The value of myCharVar is #
+The value of myStringVar is Hello
+The value of myBoolVar is 1
+
+********************************* Outut ******************************** */
